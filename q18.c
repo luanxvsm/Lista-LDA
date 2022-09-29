676902b101a388b4c7e7547ee8bcc04b82a9d6d2
Luan Victor <101013674+luanxvsm@users.noreply.github.com>
2022-09-28 21:10:26 -0300

    atualizando a q18

--- q18.c
+++ q18.c
@@ -6,17 +6,38 @@ intervalo de 1 a 10. */
 
 #include <stdio.h>
 
-int main()
-{
-    int i, nota, soma, media, maior, menor;
-    
-    for(i=1; i<=10; i++){
-        printf("Digite a nota: ");
-        scanf("%d", &nota);
-    }
-    soma += nota;
-    media = soma/10;
-    
-    printf("A soma das notas é: %d\n", soma);
-    printf("A média das notas é: %d\n", media);
+int main() {
+	int soma, maior, menor, numero,i;
+	
+	printf("Entre com o 1o numero inteiro: ");
+ 	scanf("%i", &numero); 
+ 	
+ 	maior=numero;
+ 	menor=numero;
+ 	soma += numero;
+	
+	if (numero<=10){
+		for(i=1; i<10; i++){
+ 		printf("Entre com o %do numero inteiro: ",i+1);
+ 		scanf("%i", &numero);
+ 		soma += numero;
+
+ 		if(numero>maior)
+ 			maior=numero;
+ 		else
+ 			if(numero<menor)
+ 				menor=numero;
+ 		} 
+ 		
+ 	float media = (float) soma / 10;
+	printf("A média dos números é: %.2f", media);
+ 	printf("\nO menor numero entrado é: %d", menor);
+	printf("\nO maior numero entrado é: %d", maior);
+	printf("\nA soma dos números é: %i", soma);
+		
+	}
+	else{
+		printf("Número entre 0 a 10");
+	}
+	
 }
